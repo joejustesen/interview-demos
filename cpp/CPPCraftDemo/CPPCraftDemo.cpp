8f35b1ef82ee12898eaf257fb0fdb083a901d752
Joe Justesen <jcjustesen@gmail.com>
2019-09-28 15:34:11 -0400

    Add StopWatch for measuring time

--- cpp/CPPCraftDemo/CPPCraftDemo.cpp
+++ cpp/CPPCraftDemo/CPPCraftDemo.cpp
@@ -1,4 +1,5 @@
-#include "stdafx.h"
+#include <stdio.h>
+// #include <tchar.h>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -7,78 +8,104 @@
 #include <iostream>
 #include <ratio>
 
+#include "StopWatch.h"
+
 /**
     Represents a Record Object
 */
-struct QBRecord
-{
-    uint column0; // unique id column
+struct QBRecord {
+    uint        column0;			// unique id column
     std::string column1;
-    long column2;
+    long        column2;
     std::string column3;
 };
 
 /**
 Represents a Record Collections
 */
-typedef std::vector<QBRecord> QBRecordCollection;
+using QBRecordCollection = std::vector<QBRecord>;
 
 /**
     Return records that contains a string in the StringValue field
     records - the initial set of records to filter
     matchString - the string to search for
 */
-QBRecordCollection QBFindMatchingRecords(const QBRecordCollection& records, const std:string& columnName, const std::string& matchString)
-    {
+
+QBRecordCollection QBFindMatchingRecords (
+    const QBRecordCollection & records,
+	const std::string & columnName,
+	const std::string & matchString)
+{
     QBRecordCollection result;
-    std::copy_if(records.begin(), records.end(), std::back_inserter(result), [&](QBRecord rec){
-        if (columnName == "column0") {
-            uint matchValue = std::stoul(matchString);
+
+    std::copy_if(records.begin(), records.end(), std::back_inserter(result), [columnName, matchString](QBRecord rec) {
+		
+		if (columnName == "column0") {
+            uint matchValue = std::stoul (matchString);
             return matchValue == rec.column0;
-        } else if (columnName == "column1") {
+		} else if (columnName == "column1") {
             return rec.column1.find(matchString) != std::string::npos;
-        } else if (columnName == "column2") {
-            long matchValue = std::stol(matchString);
+            
+		} else if (columnName == "column2") {
+            long matchValue = std::stol (matchString);
             return matchValue == rec.column2;
-        } else if (columnName == "column3") {
-            return rec.column3.find(matchString) != std::string::npos;
-        } else {
+		} else if (columnName == "column3"){
+            return rec.column3.find (matchString) != std::string::npos;
+		} else {
             return false;
-        }
+		}
     });
+
     return result;
-    }
+}
 
 /**
     Utility to populate a record collection
     prefix - prefix for the string value for every record
     numRecords - number of records to populate in the collection
 */
-QBRecordCollection populateDummyData(const std::string& prefix, int numRecords)
-    {
+QBRecordCollection populateDummyData(const std::string & prefix, int numRecords)
+{
     QBRecordCollection data;
+    
     data.reserve(numRecords);
-    for (uint i = 0; i < numRecords; i++)
-        {
-        QBRecord rec = { i, prefix + std::to_string(i), i % 100, std::to_string(i) + prefix };
+
+    for (auto i = 0; i < numRecords; ++i) {
+        QBRecord rec = { i, prefix + std::to_string (i), i % 100,
+        std::to_string (i) + prefix };
+        
         data.emplace_back(rec);
-        }
-    return data;
     }
 
-int main(int argc, _TCHAR* argv[])
-{
-    using namespace std::chrono;
-    // populate a bunch of data
-    auto data = populateDummyData("testdata", 1000);
-    // Find a record that contains and measure the perf
-    auto startTimer = steady_clock::now();
-    auto filteredSet = QBFindMatchingRecords(data, "column1", "testdata500");
-    auto filteredSet2 = QBFindMatchingRecords(data, "column2", "24");
-    std::cout << "profiler: " << double((steady_clock::now() - startTimer).count()) * steady_clock::period::num / steady_clock::period::den << std::endl;
-
-    // make sure that the function is correct
-    assert(filteredSet.size() == 1);
-	return 0;
+    return data;
 }
 
+// create set of tests to validate the implementation first
+
+
+int main ()
+{
+    using namespace std::chrono;
+    
+    
+    // populate a bunch of data
+    auto data = populateDummyData("testdata", 1000);
+  // Find a record that contains and measure the perf
+ 
+    auto stopWatch = StopWatch{};
+    
+    // auto startTimer = steady_clock::now();
+    
+    auto filteredSet = QBFindMatchingRecords (data, "column1", "testdata500");
+    auto filteredSet2 = QBFindMatchingRecords (data, "column2", "24");
+    
+    // std::cout << "profiler: " << double((steady_clock::now () - startTimer).count ()) * steady_clock::period::num / steady_clock::period::den << std::endl;
+    std::cout << "duration: " << std::fixed << stopWatch.seconds() << "s\n";
+    
+    
+
+    // make sure that the function is correct
+    assert (filteredSet.size () == 1);
+  
+    return 0;
+}
---
+++ cpp/CPPCraftDemo/StopWatch.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <chrono>
+
+class StopWatch {
+    using Clock = std::chrono::high_resolution_clock;
+    using TimePoint = Clock::time_point;
+    using Duration = Clock::duration;
+
+    TimePoint d_start;
+    TimePoint d_end;
+
+public:
+    StopWatch() : d_start{ Clock::now() } {}
+    
+    template <typename DUR>
+    DUR duration() const {
+        auto end = d_end.time_since_epoch() != Duration::zero() ? d_end : Clock::now();
+        return std::chrono::duration_cast<DUR>(end - d_start);
+    }
+
+    double seconds() const {
+        return duration<std::chrono::nanoseconds>().count() / 1'000'000'000.0;
+    }
+    
+    inline void stop() {
+        d_end = Clock::now();
+    }
+
+    inline void start() {
+        d_end = TimePoint{};
+    }
+};
